cfc96f2dee2191dbefdf5fd0b9fdc3de638306a8
plkmnb100 <pcy06@kakao.com>
2025-03-24 21:31:12 +0900

    feat: add theme and font in main layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,30 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { 
+  DarkTheme as NativeDarkTheme,
+  DefaultTheme as NativeDefaultTheme,
+  ThemeProvider as NativeThemeProvider 
+} from '@react-navigation/native';
+
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import { DarkTheme, LightTheme } from '@/constants/theme';
+
+import { useColorScheme } from 'react-native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const StyledTheme = colorScheme === 'dark' ? DarkTheme : LightTheme
+  const NativeTheme = colorScheme === 'dark' ? NativeDarkTheme : NativeDefaultTheme
+
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    'SUIT Variable': require('../assets/fonts/SUIT-Variable.ttf'),
   });
 
   useEffect(() => {
@@ -27,13 +37,20 @@ export default function RootLayout() {
     return null;
   }
 
+  const isLoggedIn = false;
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <NativeThemeProvider value={NativeTheme}>
+      <StyledThemeProvider theme={StyledTheme}>
+        <Stack>
+          {isLoggedIn ? (
+            null
+          ) : (
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          )}
+        </Stack>
+        <StatusBar style="auto" />
+      </StyledThemeProvider>
+    </NativeThemeProvider>
   );
 }
